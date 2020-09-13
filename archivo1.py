3e8681f6c9d1fcaa66b2fdcdcd83f2cc12745732
Hans Adasme <hans.adasme.v@mail.pucv.cl>
2020-09-13 07:21:15 -0300

    la tarea esta lista

---
+++ archivo1.py
@@ -0,0 +1,10 @@
+x = 3
+y = 7
+print('si las variables:')
+print(f'x, vale {x}')
+print(f'y, valen {y}')
+print(f'la suma de 3 mas 7 es:{x}+{y}={x+y}')
+print(f'la resta de 3 menos 7 es:{x}-{y}={x-y}')
+print(f'la multiplicacion de 7 por 3 es:{y}x{x}={y*x}')
+print(f'y la division de 7 entre 3 es:{y}/{x}={y/x}')
+print(f'quedando esta ultima con resto {y%x}')
